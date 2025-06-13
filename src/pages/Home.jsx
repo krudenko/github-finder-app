7f77040fc3ea965f92590e7f3d9bed4dd993dd09
Kseniia Rudenko <ksenia1711@gmail.com>
2025-06-13 15:57:16 +0200

    Attemp to add Daisy UI (not working yet)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 function Home() {
   return (
     <div>
-        <h1 className="text-6xl">Welcome</h1>
+      <h1 className="text-6xl">Welcome</h1>
+      {/* <div className="p-4">
+        <button className="btn btn-primary">DaisyUI Button</button>
+      </div> */}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+console.log("✅ DaisyUI plugin loaded");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"],  // you can customize or remove this line
+    darkTheme: "dark",          // set default dark theme
+  },
 };
