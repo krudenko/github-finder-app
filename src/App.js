eeb8973f49347dc19c3c591fdc3f36ca2ff7a230
Kseniia Rudenko <ksenia1711@gmail.com>
2025-06-16 12:07:50 +0200

    Added user page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar.jsx";
 import Footer from "./components/layout/Footer.jsx";
+import Alert from "./components/layout/Alert.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import User from "./pages/User.jsx";
 import { GithubProvider } from "./context/github/GithubContext.jsx";
+import { AlertProvider } from "./context/alert/AlertContext.js";
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
 
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <>
-                    <Home />
-                  </>
-                }
-              />
-              <Route path="/about" element={<About />} />
-              <Route path="/notfound" element={<NotFound />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+            <main className="container mx-auto px-3 pb-12">
+              <Alert />
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <Home />
+                    </>
+                  }
+                />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,5 +1,3 @@
-import loading from "daisyui/components/loading"
-
 const githubReducer = (state, action) => {
     switch(action.type) {
         case 'GET_USERS':
@@ -8,11 +6,23 @@ const githubReducer = (state, action) => {
                 users: action.payload,
                 loading: false,
             }
+        case 'GET_USER':
+            return {
+                ...state,
+                user: action.payload,
+                loading: false,
+            }    
         case 'CLEAR_USERS':
             return {
                 ...state,
                 users: [],
             }    
+        case 'GET_REPOS':
+            return {
+                ...state,
+                repos: action.payload,
+                loading: false,
+            }       
         case 'SET_LOADING':
             return {
                 ...state,
