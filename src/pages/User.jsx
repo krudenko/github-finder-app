85cd99ea89c3d40f354991f881883d2d6ea45e6e
Kseniia Rudenko <ksenia1711@gmail.com>
2025-06-16 15:57:48 +0200

    Moved logic to GithubActions

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,16 +5,21 @@ import { useParams } from "react-router-dom";
 import Spinner from "../components/layout/Spinner";
 import RepoList from "../components/repos/RepoList";
 import GithubContext from "../context/github/GithubContext";
+import { getUserAndRepos } from "../context/github/GithubActions";
 
 function User() {
-  const { getUser, user, loading, getUserRepos, repos } = useContext(GithubContext);
+  const { user, loading, repos, dispatch } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
-  }, []);
+    dispatch({ type: "SET_LOADING" });
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: "GET_USER_AND_REPOS", payload: userData });
+    };
+    getUserData()
+  }, [dispatch, params.login]);
 
   const {
     name,
@@ -168,12 +173,11 @@ function User() {
                   {public_gists}
                 </div>
               </div>
-
             </div>
           </div>
         </div>
 
-      <RepoList repos={repos}/>      
+        <RepoList repos={repos} />
       </div>
     </>
   );
